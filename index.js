86548a2a7756bab17eb1718fcef4e5d990a1eed6
FelipeOtero <48389979@est.ort.edu.ar>
2025-03-31 09:17:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+// console.log("tralalerotralala");
+ const array = ["agua","cactus"];
+const arrayNum=[4,6,5,4]
+
+
+
+// function mostrarFuncionTradicional() 
+// { 
+//   array.map((Number)=>{
+//     console.log(Number)
+
+
+//   });
+// }
+
+// mostrarFuncionTradicional();
+
+
+// const mostrarFuncionFlecha=()=>{
+//     array.map((Number)=>{
+//         console.log(Number)
+    
+    
+//     });
+
+// }
+
+// mostrarFuncionFlecha();
+
+// const agregarValorArray=()=>{
+// array.push("bombardiro cocodrilo");
+// mostrarFuncionFlecha();
+// }
+// agregarValorArray();
+
+// const mostrarUltimaPosicion= ()=>{
+//     let ultPosicion = array.lastIndexOf("agua");
+//     console.log(ultPosicion);
+// }
+// mostrarUltimaPosicion();
+
+// const mostrarSumatoria=()=>{
+//     let sumatoria=0;
+//     arrayNum.forEach(numero => {
+//        sumatoria+=numero; 
+//     });
+//     console.log(sumatoria);
+// }
+
+// mostrarSumatoria();
+
+// const invertirElementosArray = array.reverse();
+
+// console.log(invertirElementosArray);
+
+// const ordenarValores=()=>{
+// arrayNum.sort();
+// console.log(arrayNum);
+// }
+// ordenarValores();
+
+// let valor = 4;
+
+// function filtrarMayoresQue(array, valor) {
+//     return array.filter(num => num > valor);
+// }
+
+// console.log(filtrarMayoresQue(arrayNum,valor));
+// let contarCantVeces = arrayNum.filter(num=>num===4).length;
+
+// console.log(contarCantVeces);
