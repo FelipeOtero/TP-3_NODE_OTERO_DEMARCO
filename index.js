556e79a8e45846406c45b22a4371a90f952b5b67
FelipeOtero <48389979@est.ort.edu.ar>
2025-03-31 09:39:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,71 +1,75 @@
-// console.log("tralalerotralala");
- const array = ["agua","cactus"];
+// 1
+
+console.log("tralalerotralala");
+const array = ["agua","cactus"];
 const arrayNum=[4,6,5,4]
 
+// 2
+function mostrarFuncionTradicional() 
+{ 
+  array.map((Number)=>{
+    console.log(Number)
+});
+}
+
+mostrarFuncionTradicional();
 
 
-// function mostrarFuncionTradicional() 
-// { 
-//   array.map((Number)=>{
-//     console.log(Number)
 
 
-//   });
-// }
-
-// mostrarFuncionTradicional();
-
-
-// const mostrarFuncionFlecha=()=>{
-//     array.map((Number)=>{
-//         console.log(Number)
+ const mostrarFuncionFlecha=()=>{
+     array.map((Number)=>{
+         console.log(Number)
     
     
-//     });
+     });
 
-// }
+ }
 
-// mostrarFuncionFlecha();
+ mostrarFuncionFlecha();
+//3
+ const agregarValorArray=()=>{
+ array.push("bombardiro cocodrilo");
+ mostrarFuncionFlecha();
+ }
 
-// const agregarValorArray=()=>{
-// array.push("bombardiro cocodrilo");
-// mostrarFuncionFlecha();
-// }
-// agregarValorArray();
+ 
+ agregarValorArray();
+ //4
 
-// const mostrarUltimaPosicion= ()=>{
-//     let ultPosicion = array.lastIndexOf("agua");
-//     console.log(ultPosicion);
-// }
-// mostrarUltimaPosicion();
+ const mostrarUltimaPosicion= ()=>{
+     let ultPosicion = array.lastIndexOf("agua");
+     console.log(ultPosicion);
+ }
+ mostrarUltimaPosicion();
+ //5
 
-// const mostrarSumatoria=()=>{
-//     let sumatoria=0;
-//     arrayNum.forEach(numero => {
-//        sumatoria+=numero; 
-//     });
-//     console.log(sumatoria);
-// }
+ const mostrarSumatoria=()=>{
+     let sumatoria=0;
+     arrayNum.forEach(numero => {
+        sumatoria+=numero; 
+     });
+     console.log(sumatoria); }
 
-// mostrarSumatoria();
+ mostrarSumatoria();
+ //6
 
-// const invertirElementosArray = array.reverse();
+ const invertirElementosArray = array.reverse();
 
-// console.log(invertirElementosArray);
+ console.log(invertirElementosArray);
+//7
+ const ordenarValores=()=>{
+ arrayNum.sort();
+ console.log(arrayNum);
+ }
+ ordenarValores();
+//8
+ let valor = 4;
+ function filtrarMayoresQue(array, valor) {
+     return array.filter(num => num > valor);
+ }
+//9
+ console.log(filtrarMayoresQue(arrayNum,valor));
+ let contarCantVeces = arrayNum.filter(num=>num===4).length;
 
-// const ordenarValores=()=>{
-// arrayNum.sort();
-// console.log(arrayNum);
-// }
-// ordenarValores();
-
-// let valor = 4;
-
-// function filtrarMayoresQue(array, valor) {
-//     return array.filter(num => num > valor);
-// }
-
-// console.log(filtrarMayoresQue(arrayNum,valor));
-// let contarCantVeces = arrayNum.filter(num=>num===4).length;
-
-// console.log(contarCantVeces);
+ console.log(contarCantVeces);
